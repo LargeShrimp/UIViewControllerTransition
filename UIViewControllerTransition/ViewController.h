79d9687e4a981d7c8b4c4c329cfa740fe02611b3
taitanxiami <taitanxiami@gmail.com>
2015-12-14 10:15:50 +0800

    Initial Commit

---
+++ UIViewControllerTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewControllerTransition
+//
+//  Created by taitanxiami on 15/12/14.
+//  Copyright © 2015年 taitanxiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIViewControllerTransition/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIViewControllerTransition
+//
+//  Created by taitanxiami on 15/12/14.
+//  Copyright © 2015年 taitanxiami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
